5a3367832a677b2d68fced310e03c3cf8da4f19c
Trey Miller <Trey_M@heitkamp.corp>
2018-09-21 13:08:40 -0500

    Began Work on Refactoring to support regex

--- pygrep.py
+++ pygrep.py
@@ -1,25 +1,47 @@
-import argparse, re
+import argparse, re, sys
 
 # handle arguments
-parser = argparse.ArgumentParser()
-parser.add_argument('-v', '--verbose', help='log more verbosely', action='store_true')
+parser = argparse.ArgumentParser(
+    description='A grep clone written in python')
+
 parser.add_argument('file', help='file to search for matching pattern')
-parser.add_argument('pattern', help='pattern to search input file for matching lines')
+parser.add_argument('pattern', nargs='?', default='', help='pattern to search input file for matching lines')
+parser.add_argument('-e', '--expression', action='append', help='pattern to search input file for matching lines')
+parser.add_argument('-v', '--verbose', help='log more verbosely', action='store_true')
 
-args = parser.parse_args()
+args = parser.parse_args() 
+ 
+def outputLinesIncludingPattern():
+    
+    # Open File and Print out matching lines
+    file = open(args.file, 'r')
 
-# Alert if started in verbose mode
-if(args.verbose):
-    print('Verbose Mode Enabled')
-    print('Input File: ', args.file)
-    print('Pattern: ', args.pattern)
+    for line in file:
+        if line.lower().find(args.pattern.lower()) > 0:
+            print('>',line)
 
+    file.close()
 
-# Open File and Print out matching lines
-file = open(args.file, 'r')
+    return 
 
-for line in file:
-    if line.lower().find(args.pattern.lower()) > 0:
-        print('>',line)
+# need to refactor to use regular expression
+def outputLinesMatchingRegularExpressions():
+    
+    file = open(args.file, 'r')
 
-file.close()
+    for line in file:
+        if line.lower().find(args.pattern.lower()) > 0:
+            print('>',line)
+
+    file.close()    
+
+    return
+
+# Check to see if using regular expression
+if args.pattern != '': 
+    outputLinesIncludingPattern()
+elif args.expression:
+    outputLinesMatchingRegularExpressions()
+elif args.pattern != '' and not args.expression:
+    sys.exit('No Valid Pattern Found')
+
